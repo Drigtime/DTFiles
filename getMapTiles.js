ba4647b0e593b42edd86f7fa2105cb0039c5136a
William <william.varlet8020@gmail.com>
2019-11-04 14:44:36 +0100

    download map tile is working & up to date

--- getMapTiles.js
+++ getMapTiles.js
@@ -4,54 +4,54 @@
 // https://ankama.akamaized.net/games/dofus-tablette/assets/2.24.6/gfx/maps/1-0.2-1.jpg - 8x7
 // https://ankama.akamaized.net/games/dofus-tablette/assets/2.28.4_T99'1'Q2hkeFvVmjPpzcjlR7I*d2eWg*/gfx/maps/1-1-1.jpg
 
-const fetch = require('node-fetch');
-const fs = require('graceful-fs');
+const fetch = require("node-fetch");
+const fs = require("graceful-fs");
 
 const worlds = [
   {
-    label: 'amakna',
+    label: "amakna",
     worldId: 1,
     zooms: [
-      { label: '1', maxHorizontalTile: 40, maxVerticalTile: 32 },
-      { label: '0.6', maxHorizontalTile: 24, maxVerticalTile: 20 },
-      { label: '0.4', maxHorizontalTile: 16, maxVerticalTile: 13 },
-      { label: '0.2', maxHorizontalTile: 8, maxVerticalTile: 7 }
+      { label: "1", maxHorizontalTile: 40, maxVerticalTile: 32 },
+      { label: "0.6", maxHorizontalTile: 24, maxVerticalTile: 20 },
+      { label: "0.4", maxHorizontalTile: 16, maxVerticalTile: 13 },
+      { label: "0.2", maxHorizontalTile: 8, maxVerticalTile: 7 }
     ]
   },
   {
-    label: 'incarnam',
+    label: "incarnam",
     worldId: 2,
     zooms: [
-      { label: '1', maxHorizontalTile: 20, maxVerticalTile: 12 },
-      { label: '0.5', maxHorizontalTile: 10, maxVerticalTile: 6 },
-      { label: '0.25', maxHorizontalTile: 5, maxVerticalTile: 3 }
+      { label: "1", maxHorizontalTile: 20, maxVerticalTile: 12 },
+      { label: "0.5", maxHorizontalTile: 10, maxVerticalTile: 6 },
+      { label: "0.25", maxHorizontalTile: 5, maxVerticalTile: 3 }
     ]
   }
 ];
-const path = {};
+
 const promises = [];
 
-const getMapTile = (world, zoom, col) => {
+const getMapTile = (world, zoom, col, path) => {
   return new Promise((resolve, reject) => {
-    fetch(
-      `https://ankama.akamaized.net/games/dofus-tablette/assets/2.28.4_T99'1'Q2hkeFvVmjPpzcjlR7I*d2eWg*/gfx/maps/${world.worldId}-${zoom.label}-${col}.jpg`
-    )
+    const filePath = `./map/${world.label}/${zoom.label}/${path.horizontal}/${path.vertical}.jpg`;
+    const url = `https://ankama.akamaized.net/games/dofus-tablette/assets/2.28.4_T99'1'Q2hkeFvVmjPpzcjlR7I*d2eWg*/gfx/maps/${world.worldId}-${zoom.label}-${col}.jpg`;
+    fetch(url)
       .then(res => {
-        const req = res.body.pipe(
-          fs.createWriteStream(
-            `./map/${world.label}/${zoom.label}/${path.horizontal}/${path.vertical}.jpg`
-          )
-        );
-        req.on('finish', () => {
-          resolve(`./map/${world.label}/${zoom.label}/${path.horizontal}/${path.vertical}.jpg`);
+        const stream = fs.createWriteStream(filePath);
+        const req = res.body.pipe(stream);
+        req.on("finish", () => {
+          console.log(filePath);
+          resolve(filePath);
         });
       })
-      .catch(res => console.log(res));
+      .catch(res => {
+        console.log(`${res} downloaded`);
+        reject();
+      });
   });
 };
 
 worlds.forEach(world => {
-  console.log(world.label);
   world.zooms.forEach(zoom => {
     console.log(zoom.label);
     if (!fs.existsSync(`./map`)) fs.mkdirSync(`./map`);
@@ -65,6 +65,10 @@ worlds.forEach(world => {
       horizontal++
     ) {
       let col = 1 * horizontal;
+      const path = {
+        horizontal: null,
+        vertical: null
+      };
       path.horizontal =
         10 > horizontal
           ? (horizontal = `00${horizontal}`)
@@ -81,11 +85,13 @@ worlds.forEach(world => {
         ) {
           fs.mkdirSync(`./map/${world.label}/${zoom.label}/${path.horizontal}`);
         }
-        promises.push(getMapTile(world, zoom, col));
+        promises.push(getMapTile(world, zoom, col, path));
         col = col + zoom.maxHorizontalTile;
       }
     }
   });
 });
 
-Promise.all(promises).then((res) => console.log(res));
+Promise.all(promises).then(() =>
+  console.log("All map tile have been downloaded")
+);
